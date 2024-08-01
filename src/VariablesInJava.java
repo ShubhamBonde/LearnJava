d6d8ca0939a81b4c5a6c5948b0f24623a585afbd
Shubham Bonde <shubhambonde2000@gmail.com>
2024-08-02 01:03:09 +0530

    Added variables file

---
+++ src/VariablesInJava.java
@@ -0,0 +1,19 @@
+public class VariablesInJava {
+    public static void main(String[] args) {
+
+        // I'm defining variable called 'num' here.
+        int num = 0;
+      
+        System.out.println(num);
+    }
+
+    /**
+     * Notes:
+     * - Variables in java are case sensitive
+     * - Variables cannot start with a number. You can use $, _, a-z, A-Z
+     * - Subsequent characters in the variable identifier can contain alphabets, digits, $ or _
+     * - Reserved keywords in Java can't be used as variable identifier.
+     * - Conventions: camelCase is used in java to name a variable
+     * - Constants must be defined in capital letters with distinct words separated by _. Ex. MAX_LENGTH, CLIENT_SIDE_URL
+     */
+}
